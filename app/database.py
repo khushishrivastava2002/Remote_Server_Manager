cd1ea90c287a4981b9044ce78e6c7dc732b6eef6
khushi <shrivastavakhushi419@gmail.com>
2025-12-26 13:10:20 +0530

    removed cred

--- app/database.py
+++ app/database.py
@@ -8,8 +8,19 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
+    # Email Settings
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str = "smtp.gmail.com"
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
+
     class Config:
-        env_file = ".env"
+        env_file = (".env", ".env.local")
 
 settings = Settings()
 
--- app/routers/email.py
+++ app/routers/email.py
@@ -10,15 +10,15 @@ router = APIRouter()
 
 # Email Configuration
 conf = ConnectionConfig(
-    MAIL_USERNAME="shrivastavakhushi419@gmail.com",
-    MAIL_PASSWORD="plor ylgf mjtu pfnh",
-    MAIL_FROM="shrivastavakhushi419@gmail.com",
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True
+    MAIL_USERNAME=settings.MAIL_USERNAME,
+    MAIL_PASSWORD=settings.MAIL_PASSWORD,
+    MAIL_FROM=settings.MAIL_FROM,
+    MAIL_PORT=settings.MAIL_PORT,
+    MAIL_SERVER=settings.MAIL_SERVER,
+    MAIL_STARTTLS=settings.MAIL_STARTTLS,
+    MAIL_SSL_TLS=settings.MAIL_SSL_TLS,
+    USE_CREDENTIALS=settings.USE_CREDENTIALS,
+    VALIDATE_CERTS=settings.VALIDATE_CERTS
 )
 
 class EmailSchema(BaseModel):
